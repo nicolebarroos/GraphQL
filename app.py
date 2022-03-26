4d63967506d6e1878431b59e611ccc7194d6f69e
Nicole Barros <barrosnicole094@gmail.com>
2022-03-25 23:04:57 -0300

    add mutation

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ from api import app, db
 from ariadne import load_schema_from_path, make_executable_schema, graphql_sync, snake_case_fallback_resolvers, ObjectType
 from ariadne.constants import PLAYGROUND_HTML
 from flask import request, jsonify
-from api.queries import listPosts_resolver
+from api.queries import listPosts_resolver, getPost_resolver, create_post_resolver, update_post_resolver, \
+    delete_post_resolver
 
 query = ObjectType("Query")
+mutation = ObjectType("Mutation")
 query.set_field("listPosts", listPosts_resolver)
+query.set_field("getPost", getPost_resolver)
+mutation.set_field("createPost", create_post_resolver)
+mutation.set_field("updatePost", update_post_resolver)
+mutation.set_field("deletePost", delete_post_resolver)
 
 type_defs = load_schema_from_path("schema.graphql")
 
@@ -15,6 +21,7 @@ type_defs = load_schema_from_path("schema.graphql")
 schema = make_executable_schema(
     type_defs,
     query,
+    mutation,
     #Snake é um Bindable: esses são tipos especiais da biblioteca Ariadne que é usada para vincular métodos python ao esquema GraphQL.
     snake_case_fallback_resolvers
 )
